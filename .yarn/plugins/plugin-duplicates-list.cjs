760d180203f61deeee94e537e8268a1292db12c3
Anton Zhevak <antzhe@gmail.com>
2025-12-22 10:32:05 +0500

    task-2

--- .yarn/plugins/plugin-duplicates-list.cjs
+++ .yarn/plugins/plugin-duplicates-list.cjs
@@ -1,7 +1,48 @@
+// - Получить cwd и конфигурацию (Configuration)
+// - Получить project (Project)
+// - Получить state установки (restoreInstallState)
+// - При итерации фильтровать virtual (structUtils)
+// - Пройтись по storedPackages
 module.exports = {
-  name: `plugin-duplicates-list`,
-  factory: (require) => {
-    const { BaseCommand } = require(`@yarnpkg/cli`);
-    const { Configuration, Project, structUtils } = require("@yarnpkg/core");
-  },
+    name: `plugin-duplicates-list`,
+    factory: (require) => {
+        const { BaseCommand } = require(`@yarnpkg/cli`);
+        const { Configuration, Project, structUtils } = require("@yarnpkg/core");
+        const cwd = process.cwd();
+
+        const dupMap = {};
+
+        class DuplicatesCommand extends BaseCommand {
+            static paths = [['duplicates']];
+
+            async execute() {
+                const configuration = await Configuration.find(cwd, null);
+                const { project } = await Project.find(configuration, cwd);
+
+                await project.restoreInstallState();
+
+                for (let [_key, pkg] of project.storedPackages) {
+                    const pkgName = pkg.name;
+                    const pkgVer = pkg.version;
+
+                    dupMap[pkgName] ??= new Set();
+                    dupMap[pkgName].add(pkgVer);
+                }
+
+                for (let pkgName in dupMap) {
+                    const versions = dupMap[pkgName];
+
+                    if (versions.size > 1) {
+                        this.context.stdout.write(`${pkgName}: ${Array.from(versions).join(' ')}\n`);
+                    }
+                }
+            }
+        }
+
+        return {
+            commands: [
+                DuplicatesCommand,
+            ],
+        };
+    },
 };
