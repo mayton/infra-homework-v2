70dfa5f86e2d7fc18ff47f832cab32ae81178cf7
Anton Zhevak <antzhe@gmail.com>
2025-12-22 10:32:48 +0500

    task-1

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,16 @@
-const yaml = require("yaml");
+const { parse } = require("yaml");
 const fs = require("node:fs");
+
+function fn(module, filename) {
+    const content = fs.readFileSync(filename, 'utf8');
+    
+    try {
+        module.exports = parse(content);
+    } catch (err) {
+        err.message = filename + ': ' + err.message;
+        throw err;
+    }
+};
+
+require.extensions['.yml'] = fn;
+require.extensions['.yaml'] = fn;
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,18 @@
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import yaml from "yaml";
+
+export async function load(url, context, nextLoad) {
+    if (url.endsWith('.yaml') || url.endsWith('.yml')) {
+        const source = await fs.readFile(fileURLToPath(url), 'utf8');
+        const data = yaml.parse(source);
+
+        return {
+            format: 'module',
+            source: `export default ${JSON.stringify(data)};`,
+            shortCircuit: true,
+        };
+    }
+
+    return nextLoad(url, context);
+};
