24a8f82e3bb03b9fc567a5664c8759c2d2e110ca
Anton Zhevak <antzhe@gmail.com>
2025-12-28 10:03:53 +0500

    task-3

--- tasks/task-3/linter.js
+++ tasks/task-3/linter.js
@@ -5,5 +5,25 @@ import { walk } from "zimmerframe";
 export function check(filePath) {
   const errors = [];
 
+  const src = fs.readFileSync(filePath, { encoding: 'utf-8' });
+  const ast = parse(src, {
+    ecmaVersion: 'latest',
+    allowAwaitOutsideFunction: true,
+  });
+  
+  walk(ast, { errors, mapFnNameToAsync: {} }, {
+    FunctionDeclaration(node, { state }) {
+      state.mapFnNameToAsync[node.id.name] = node.async;
+    },
+    IfStatement(node, { state }) {
+      if (
+        node.test.type === 'CallExpression' &&
+        state.mapFnNameToAsync[node.test.callee.name]
+      ) {
+        state.errors.push({ start: node.test.start, end: node.test.end });
+      }
+    },
+  });
+
   return errors;
 }
